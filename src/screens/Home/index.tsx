b69403a04fec5dbceb53548226e8779e88212038
Ventura Junior <you@example.com>
2023-08-11 20:33:55 -0300

    Add text input and button elements

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,29 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import { styles } from './styles'
 
 export function Home() {
+
+  function handleparticipantAdd() {
+    console.log('Voce clicou no botão de adicionar')
+  }
   return (
     <View style={styles.container}>
-      <Text style={styles.eventName}>Ventura</Text>
-      <Text style={styles.eventDate}>Frontend Developer</Text>
+      <Text style={styles.eventName}>Nome do Evento</Text>
+      <Text style={styles.eventDate}>11 de Agosto, sexta-feira, 2023</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor="#b8b8b8"
+        keyboardAppearance="dark"
+        keyboardType="email-address"
+      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={handleparticipantAdd}
+      >
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
     </View>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -4,19 +4,40 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#5a035a",
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 16
   },
   eventName: {
     color: '#FDFCFE',
     fontSize: 32,
     fontWeight: 'bold',
-    marginTop: 48
+    marginTop: 48,
   },
   eventDate: {
     color: '#6B6B6B',
     fontSize: 24,
     fontWeight: 'bold',
-    marginTop: 48
+    marginTop: 24,
+  },
+  input: {
+    marginTop: 16,
+    height: 56,
+    borderRadius: 8,
+    padding: 16,
+    backgroundColor: '#212122',
+    color: '#fff',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 24
+  },
+  button: {
+    marginTop: 16,
+    width: 56,
+    height: 56,
+    borderRadius: 8,
+    backgroundColor: '#31CF67',
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 })
