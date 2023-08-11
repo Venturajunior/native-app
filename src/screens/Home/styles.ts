46831f76854fb9232a419b0e1c6c2db74a641f67
Ventura Junior <you@example.com>
2023-08-11 19:15:21 -0300

    Initial project structure

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#5a035a",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  eventName: {
+    color: '#FDFCFE',
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginTop: 48
+  },
+  eventDate: {
+    color: '#6B6B6B',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48
+  }
+})
